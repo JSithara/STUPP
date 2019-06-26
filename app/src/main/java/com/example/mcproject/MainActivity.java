5f96b62b2486dd5c60ce321179724bb6b7d41c9c
Sithara Jayachandran <st962427@dal.ca>
2019-06-26 13:29:43 -0300

    added activities

--- app/src/main/java/com/example/mcproject/MainActivity.java
+++ app/src/main/java/com/example/mcproject/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.mcproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,54 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ImageView Campus = findViewById(R.id.campus_icon);
+        ImageView Events = findViewById(R.id.events_icon);
+        ImageView Checklist = findViewById(R.id.check_list_icon);
+        ImageView toDoList = findViewById(R.id.to_do_list_icon);
+        ImageView places = findViewById(R.id.places_icon);
+        ImageView HelpLine = findViewById(R.id.help_line_icon);
+        Campus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent changeView = new Intent(v.getContext(),CampusMapActivity.class);
+                startActivity(changeView);
+            }
+        });
+        Events.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent changeView = new Intent(v.getContext(), EventsActivity.class);
+                startActivity(changeView);
+            }
+        });
+        Checklist.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent changeView = new Intent(v.getContext(),CheckListActivity.class);
+                startActivity(changeView);
+            }
+        });
+        toDoList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent changeView = new Intent(v.getContext(),ToDoListActivity.class);
+                startActivity(changeView);
+            }
+        });
+        places.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent changeView = new Intent(v.getContext(), PlacesActivity.class);
+                startActivity(changeView);
+            }
+        });
+        HelpLine.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent changeView = new Intent(v.getContext(),HelpLineActivity.class);
+                startActivity(changeView);
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/com/example/mcproject/ToDoListActivity.java
@@ -0,0 +1,12 @@
+package com.example.mcproject;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class ToDoListActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.to_do_list_activity);
+    }
+}
